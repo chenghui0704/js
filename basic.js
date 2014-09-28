8a3de849932619f53bb8c8ce3b45ba333dc2e564
akimotobeloved <akimoto@akimoto.local>
2014-09-28 16:58:21 +0800

    ＝＝

--- basic.js
+++ basic.js
@@ -7,3 +7,8 @@ var url="${rc.contextPath}/device/getuserdevices.json?"+parameters;
 util.ajax(url,{},function(info){
 	
 });
+
+//在离开页面或者刷新页面时执行绑定的事件
+$(window).bind('beforeunload', function() {
+
+});
--- 代码拾遗.js
+++
@@ -1,4 +0,0 @@
-//在离开页面或者刷新页面时执行绑定的事件
-$(window).bind('beforeunload', function() {
-
-});
