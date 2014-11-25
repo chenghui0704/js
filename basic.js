86fe5052db27ee9a76b6ea1cf8cb4de3bbd7dbe7
akimotobeloved <akimoto@akimoto.local>
2014-11-25 11:41:15 +0800

    ==

--- basic.js
+++ basic.js
@@ -51,11 +51,15 @@ function do(){
 		$("#admin-error").text("用户名或密码不能为空");
 	}
 }
+//数组插入元素
+Array.prototype.insert = function (index, item) {
+  this.splice(index, 0, item);
+};
 
 /* 
  *  方法:Array.remove(dx) 
  *  功能:根据元素值删除数组元素. 
- *  参数:元素值 
+ *  参数:元素值
  *  返回:在原数组上修改数组 
  *  作者：pxp 
  */  
@@ -99,3 +103,39 @@ $(document).ready(function() {
 var type=util.getQueryString('type');
 $("#searchKey").val(type==null?1:type);
 //end
+
+
+// Prevent the form from submitting
+$( "form" ).submit(function( event ) {
+    event.preventDefault();
+});
+
+//将class为time的节点值变成时间型数据
+$(".time").each(function(){
+	if($(this).html()=="0"||$(this).html()==""){
+		$(this).html("暂无");
+	}else
+		$(this).html(util.getDateTime(parseInt($(this).html().replace(/,/ig,''))));
+});
+
+//类数组对象转换为数组对象
+function list() {
+  return Array.prototype.slice.call(arguments, 0);
+}
+
+var list1 = list(1, 2, 3); // [1, 2, 3]
+
+
+//清空数组
+var myArray = yourArray = [1, 2, 3];
+myArray = [];//仍然是 [1, 2, 3]
+myArray.length = 0;// 正确的方法是保持引用
+
+//合并数组
+var mergeTo = [4,5,6];
+var mergeFrom = [7,8,9];
+Array.prototype.push.apply(mergeTo, mergeFrom);
+
+
+
+
